2f877db55a152b254dd8553dcba975082845d7dd
Mazuh <marcell-mz@hotmail.com>
2017-11-01 00:07:22 -0300

    Fix pep8 warnings (style warnings only)

--- editor_grafico.py
+++ editor_grafico.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-#-*- coding: utf8 -*-
+# -*- coding: utf8 -*-
 """
 Program that reads a sequence of commands to handle a MxN matrix of pixels.
 """
 
 import re
 
+
 class Image(object):
     """
     Matrix of pixels.
 
-    For public methods awareness, its indexes are from 1 up to its dimensions (inclusive ranges).
+    For public methods awareness, its indexes are from 1 up to
+    its dimensions (inclusive ranges).
     """
     _BLANK_COLOR = 'O'
 
@@ -22,12 +24,11 @@ class Image(object):
         'L': r'^L (?P<X>[0-9]+) (?P<Y>[0-9]+) (?P<C>[A-Z])$',
         'V': r'^V (?P<X>[0-9]+) (?P<Y1>[0-9]+) (?P<Y2>[0-9]+) (?P<C>[A-Z])$',
         'H': r'^H (?P<X1>[0-9]+) (?P<X2>[0-9]+) (?P<Y>[0-9]+) (?P<C>[A-Z])$',
-        'K': r'^K (?P<X1>[0-9]+) (?P<Y1>[0-9]+) (?P<X2>[0-9]+) (?P<Y2>[0-9]+) (?P<C>[A-Z])$',
+        'K': r'^K (?P<X1>[0-9]+) (?P<Y1>[0-9]+) (?P<X2>[0-9]+) (?P<Y2>[0-9]+) (?P<C>[A-Z])$',  # noqa
         'F': r'^F (?P<X>[0-9]+) (?P<Y>[0-9]+) (?P<C>[A-Z])$',
         'S': r'^S (?P<Name>.*)$',
         'X': r'^X$'
     }
-
 
     def __init__(self, cols_qtt: int, rows_qtt: int):
         """Create a new matrix with blank pixels."""
@@ -40,31 +41,30 @@ class Image(object):
             for _ in range(rows_qtt):
                 self.__matrix[col_i].append(Image._BLANK_COLOR)
 
-
     def clear(self):
         """Leave all pixels blank."""
         for row_i in range(self.__rows_qtt):
             for col_i in range(self.__cols_qtt):
                 self.__matrix[col_i][row_i] = Image._BLANK_COLOR
 
-
     def get_color(self, col: int, row: int):
         """Return a pixel value from the user given coordinates.
 
-        Note that this is the recommended access interface for most methods public or not, cause
-        its parameters most be from 1 up to their limits (and NOT begining by 0)."""
+        Note that this is the recommended access interface for most
+        methods public or not, cause its parameters most be from 1 up to
+        their limits (and NOT begining by 0)."""
         return self.__matrix[col-1][row-1]
-
 
     def set_color(self, col: int, row: int, color: str):
         """Update a single pixel value at the user given coordinates.
 
-        Note that this is the recommended access interface for most methods public or not, cause
-        its parameters most be from 1 up to their limits (and NOT begining by 0)."""
+        Note that this is the recommended access interface for most
+        methods public or not, cause its parameters most be from 1 up to
+        their limits (and NOT begining by 0)."""
         self.__matrix[col-1][row-1] = color
 
-
-    def draw_vertical(self, col: int, begining_row: int, ending_row: int, color: str):
+    def draw_vertical(self, col: int, begining_row: int,
+                      ending_row: int, color: str):
         """Update an entire vertical line."""
         if begining_row > ending_row:
             begining_row, ending_row = ending_row, begining_row
@@ -72,8 +72,8 @@ class Image(object):
         for row_i in range(begining_row, ending_row+1):
             self.set_color(col, row_i, color)
 
-
-    def draw_horizontal(self, row: int, begining_col: int, ending_col: int, color: str):
+    def draw_horizontal(self, row: int, begining_col: int, ending_col: int,
+                        color: str):
         """Update an entire horizontal line."""
         if begining_col > ending_col:
             begining_col, ending_col = ending_col, begining_col
@@ -81,18 +81,21 @@ class Image(object):
         for col_i in range(begining_col, ending_col+1):
             self.set_color(col_i, row, color)
 
-
-    def draw_rect(self, l_col: int, l_row: int, r_col: int, r_row: int, color: str):
-        """Draw a rectangle from a upper left coordinate up to a right bottom coordinate."""
+    def draw_rect(self, l_col: int, l_row: int, r_col: int, r_row: int,
+                  color: str):
+        """Draw a rectangle from a upper left coordinate up
+        to a right bottom coordinate."""
         for col_i in range(l_col, r_col+1):
             for row_i in range(l_row, r_row+1):
                 self.set_color(col_i, row_i, color)
 
+    def fill(self, ref_col: int, ref_row: int, new_color: str,
+             _replaceable_color=None):
+        """Paint a whole homogeneous area starting by an initial
+        reference coordinate."""
 
-    def fill(self, ref_col: int, ref_row: int, new_color: str, _replaceable_color=None):
-        """Paint a whole homogeneous area starting by an initial reference coordinate."""
-
-        if ref_col < 1 or ref_col > self.__cols_qtt or ref_row < 1 or ref_row > self.__rows_qtt:
+        if ref_col < 1 or ref_col > self.__cols_qtt \
+                or ref_row < 1 or ref_row > self.__rows_qtt:
             return
 
         if _replaceable_color is None:
@@ -113,29 +116,33 @@ class Image(object):
         self.fill(ref_col, ref_row+1, new_color, _replaceable_color)
         self.fill(ref_col, ref_row-1, new_color, _replaceable_color)
 
-
     def save(self, filename: str):
         """Save all matrix data on a given filename."""
         with open(filename, 'w') as saving_file:
             saving_file.write(str(self))
 
-
     def cli_exec(self, raw_command_line: str):
-        """Interpret a command line and, if it's valid, apply such command on this object.
+        """Interpret a command line and, if it's valid, apply such command
+        on this object.
 
-        For good practices reasons, this eval function will not create another instance
-        of image nor replace the instance itself, i.e., the self pointer will remain constant.
+        For good practices reasons, this eval function will not create
+        another instance of image nor replace the instance itself,
+        i.e., the self pointer will remain constant.
         That said, static method init_by_cli(str) may be useful.
 
-        Return True if could find a valid command and its parameters, otherwise False.
+        Return True if could find a valid command and its parameters,
+        otherwise False.
         """
         line = raw_command_line.strip()
 
         cmd_match = re.match(r'^(?P<cmd>[A-Z]).*', line)
         if cmd_match is None:
             return False
+
         cmd = cmd_match.group('cmd')
-        if (cmd == Image.__INIT_COMMAND_KEY) or (cmd not in Image.__AVAILABLE_CLI):
+        if cmd == Image.__INIT_COMMAND_KEY:
+            return False
+        if cmd not in Image.__AVAILABLE_CLI:
             return False
 
         argsm = re.match(Image.__AVAILABLE_CLI[cmd], line)
@@ -187,7 +194,6 @@ class Image(object):
 
         return True
 
-
     @staticmethod
     def init_by_cli(raw_command_line):
         """Automate (only the) constructor by a command line interface.
@@ -207,7 +213,6 @@ class Image(object):
 
         return Image(int(cmd.group('M')), int(cmd.group('N')))
 
-
     def __str__(self):
         drawing = ''
 
@@ -226,7 +231,6 @@ def main():
         matrix = Image.init_by_cli(input())
     while True:
         matrix.cli_exec(input())
-        print(matrix)
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#-*- coding: utf8 -*-
+# -*- coding: utf8 -*-
 """
 Unit testing.
 """
@@ -10,6 +10,7 @@ from os import remove
 
 from editor_grafico import Image
 
+
 class ImageTest(TestCase):
     """Test the Image class."""
 
@@ -19,8 +20,10 @@ class ImageTest(TestCase):
         matrix = Image.init_by_cli('I 5 6')
         matrix.cli_exec('L 2 3 A')
         matrix.cli_exec('S one.bmp')
-        self.assertTrue(cmp('one.bmp', 'tests_outputs/test_1/one.bmp', shallow=0),
-                        'Unexpected file content for one.bmp.')
+        self.assertTrue(
+            cmp('one.bmp', 'tests_outputs/test_1/one.bmp', shallow=0),
+            'Unexpected file content for one.bmp.'
+        )
         remove('one.bmp')
 
         matrix.cli_exec('G 2 3 J')
@@ -28,13 +31,14 @@ class ImageTest(TestCase):
         matrix.cli_exec('H 3 4 2 Z')
         matrix.cli_exec('F 3 3 J')
         matrix.cli_exec('S two.bmp')
-        self.assertTrue(cmp('two.bmp', 'tests_outputs/test_1/two.bmp', shallow=0),
-                        'Unexpected file content for two.bmp.')
+        self.assertTrue(
+            cmp('two.bmp', 'tests_outputs/test_1/two.bmp', shallow=0),
+            'Unexpected file content for two.bmp.'
+        )
         remove('two.bmp')
 
         with self.assertRaises(SystemExit):
             matrix.cli_exec('X')
-
 
     def test_2(self):
         """Second expected behaviour."""
@@ -48,8 +52,10 @@ class ImageTest(TestCase):
         matrix.cli_exec('K 2 7 8 8 E')
         matrix.cli_exec('F 9 9 R')
         matrix.cli_exec('S one.bmp')
-        self.assertTrue(cmp('one.bmp', 'tests_outputs/test_2/one.bmp', shallow=0),
-                        'Unexpected file content for one.bmp.')
+        self.assertTrue(
+            cmp('one.bmp', 'tests_outputs/test_2/one.bmp', shallow=0),
+            'Unexpected file content for one.bmp.'
+        )
         remove('one.bmp')
 
         with self.assertRaises(SystemExit):
